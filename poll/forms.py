febef75f9cba1e6bbcdaa2d9697bde80026f954d
veerugithubrit <112077451+veerugithubrit@users.noreply.github.com>
2023-05-06 11:29:49 +0530

    First Commit
    first date :06/05/2023

---
+++ poll/forms.py
@@ -0,0 +1,9 @@
+from optparse import Option
+from django.forms import ModelForm
+
+from .models import Poll
+
+class CreatePollForm(ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question', 'option_one', 'option_two', 'option_three', 'option_four']
